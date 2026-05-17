900879d3d640eb3ab67897b37fa85defe2f3ce48
Tasha Hankewych <thankewych@gmail.com>
2026-05-17 01:10:50 -0500

    Character can face 4 directions

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,11 +6,17 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 movement;
     private Rigidbody2D rb2d;
+    private Animator animator;
+
+    private const string lookX = "LookX";
+    private const string lookY = "LookY";
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,5 +24,11 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.Set(InputManager.Movement.x, InputManager.Movement.y);
         rb2d.linearVelocity = movement * moveSpeed;
+
+        if (movement != Vector2.zero)
+        {
+            animator.SetFloat(lookX, movement.x);
+            animator.SetFloat(lookY, movement.y);
+        }
     }
 }
